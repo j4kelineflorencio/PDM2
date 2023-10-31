85c95c57ff36fbc764bd52623a2d56b218618a9a
j4kelineflorencio <113375243+j4kelineflorencio@users.noreply.github.com>
2023-10-31 08:59:34 -0400

    Create TelaMural.java

---
+++ TelaMural.java
@@ -0,0 +1,14 @@
+package com.example.greensolves;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Tela_Mural extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_mural);
+    }
+}
